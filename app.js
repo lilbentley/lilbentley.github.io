fefb95d195c58b5f0086bd44cb97bbee2a347fcd
lilbentley <jujuvonernst@gmail.com>
2024-05-02 18:08:55 +0200

    Fix video playback issues and improve button functionality

--- app.js
+++ app.js
@@ -1,82 +1,86 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-
+// Scene Setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(48, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ alpha: true });
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.outputEncoding = THREE.sRGBEncoding;
 document.body.appendChild(renderer.domElement);
 
-let video;
-let mixer, clock = new THREE.Clock(), isClose = false;
-let animations;  // Globally store animations
-const videoTexture = setupVideoTexture();
-loadModel();
-setupCamera();
-window.addEventListener('click', onMouseClick);
-animate();
+let mixer, clock = new THREE.Clock(), isClose = true;
+let animations; 
+renderer.outputEncoding = THREE.sRGBEncoding;
+let cameraTargetPosition = new THREE.Vector3(0, 1, 8);
+camera.position.set(0, 1, 8);   
+
+const videos = {
+    intro: createVideoTexture('video/intro.mp4'),
+    video1: createVideoTexture('video/video_1.mp4', true),
+    video2: createVideoTexture('video/video_2.mp4', true),
+    video3: createVideoTexture('video/video_3.mp4', true),
+    video4: createVideoTexture('video/video_4.mp4', true),
+    video5: createVideoTexture('video/video_5.mp4', true),
+    video6: createVideoTexture('video/video_6.mp4', true),
+    videoContact: createVideoTexture('video/video_contact.mp4', true),
+};
+
+let currentVideo = videos.intro;
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+window.addEventListener('click', onClick);
+autoStart();
+
+const material = new THREE.MeshBasicMaterial({ map: videos.intro });
 
 
-function setupVideoTexture() {
-    const introVideo = document.createElement('video');
-    introVideo.src = 'video/intro.mp4';
-    introVideo.muted = true;
+let remoteAction;
 
-    const loopVideo = document.createElement('video');
-    loopVideo.src = 'video/video_1.mp4';
-    loopVideo.loop = true;
-    loopVideo.muted = true;
 
-    let videoTexture;
-
-    introVideo.addEventListener('canplaythrough', () => {
-        introVideo.play();
-        console.log("Intro video is ready and playing.");
-    });
-
-    introVideo.addEventListener('ended', () => {
-        video = loopVideo; // Switch to the loop video
-        videoTexture.image = video; // Update the video texture
-        video.play();
-        console.log("Loop video is playing.");
-    });
-
-    video = introVideo; // Start with the intro video
-    videoTexture = new THREE.VideoTexture(video);
-    videoTexture.minFilter = THREE.LinearFilter;
-    videoTexture.magFilter = THREE.LinearFilter;
-    videoTexture.format = THREE.RGBAFormat;
-    videoTexture.encoding = THREE.sRGBEncoding;
-
-    return videoTexture;
+function createVideoTexture(url, loop = false) {
+    const video = document.createElement('video');
+    video.src = url;
+    video.loop = loop;
+    video.muted = true;  // Necessary for autoplay on most browsers
+    video.load();  // Important for setting up the initial state
+    const texture = new THREE.VideoTexture(video);
+    texture.minFilter = THREE.LinearFilter; 
+    texture.encoding = THREE.sRGBEncoding; // Avoids issues with non-power-of-2 video dimensions
+    return texture;
 }
-    
+
+
 function loadModel() {
     const loader = new GLTFLoader();
-    loader.load('tv.glb', function(gltf) {
+    loader.load('scene.glb', function(gltf) {
         scene.add(gltf.scene);
         gltf.scene.scale.set(5, 5, 5);
         gltf.scene.position.set(0, 0, 0);
         console.log('Model loaded:', gltf.scene);
 
         mixer = new THREE.AnimationMixer(gltf.scene);
-        animations = gltf.animations; // Store animations for later use
+        animations = gltf.animations; 
 
-       gltf.scene.traverse(function(node)
-       {
-    if (node.isMesh && node.material) {
-    if (node.name === "SCREEN") {
-        const screenMaterial = new THREE.MeshBasicMaterial({
-            map: videoTexture,
-            
-        });
+        remoteAction = mixer.clipAction(animations[0]);
+        remoteAction.setLoop(THREE.LoopOnce);
+        remoteAction.clampWhenFinished = true;
+
+       gltf.scene.traverse(function(node){
+          if (node.isMesh && node.material) {
+                if (node.name === "SCREEN") {
+                   const screenMaterial = new THREE.MeshBasicMaterial({map: videos.intro,});
         
-        node.material = screenMaterial;
-    }
-}
-});
+                   node.material = screenMaterial;
+        
+                   const light = new THREE.PointLight(0xffffff, 100, 2);
+                   node.getWorldPosition(light.position);
+                   light.position.z += 1;
+                   scene.add(light);
+                }
+    
+    
+            }
+       });
 
 
 
@@ -85,109 +89,190 @@ function loadModel() {
     });
 }
 
-function setupCamera() {
-    camera.position.set(0, 1, 8);
-    camera.quaternion.setFromEuler(new THREE.Euler(0, 0, 0));
-}
 
-function animateCamera() {
-    const action = mixer.clipAction(animations[0]);
-    action.setLoop(THREE.LoopOnce);
-    action.clampWhenFinished = true;
-    if (!action.isRunning()) {
-        if (isClose) {
-            action.reset();
-            action.time = action.getClip().duration;
-            action.timeScale = -2;
-        } else {
-            action.reset();
-            action.time = 0;
-            action.timeScale = 1.5;
-        }
-        action.play();
-        isClose = !isClose; // Toggle state after deciding the direction
+
+function animButton(button){
+    const buttonAction = mixer.clipAction(animations[1], button);
+    buttonAction.setLoop(THREE.LoopOnce);
+    buttonAction.clampWhenFinished = true;
+    if (!buttonAction.isRunning()) {
+        buttonAction.reset();
+        buttonAction.time = 0;
+        buttonAction.timeScale = 1;
+        buttonAction.play();
     }
 }
 
-function onMouseClick(event) {
+function switchStates(){
+    if (!remoteAction.isRunning()) {
+        if(isClose){
+            
+            cameraTargetPosition.set(1.7, 1.8, 2.5);
+    
+            remoteAction.reset();
+            remoteAction.timeScale = 1;
+            remoteAction.play();
+            
+            isClose = false;
+        } else {
+            
+            cameraTargetPosition.set(0, 1, 8);
+    
+            remoteAction.reset();
+            remoteAction.time = remoteAction.getClip().duration;
+            remoteAction.timeScale = -2;
+            remoteAction.play();
+    
+    
+            isClose = true;
+        }
+    } 
+
+}
+
+function onClick(event) {
     const mouse = new THREE.Vector2(
         (event.clientX / window.innerWidth) * 2 - 1,
         -(event.clientY / window.innerHeight) * 2 + 1
     );
-    const raycaster = new THREE.Raycaster();
+    
     raycaster.setFromCamera(mouse, camera);
+
     const intersects = raycaster.intersectObjects(scene.children, true);
     if (intersects.length > 0) {
         console.log('Touched object:', intersects[0].object.name);
-        // Check if the clicked object is one of the specific objects
-        if (intersects[0].object.name === 'FLOOR' || intersects[0].object.name === 'SCREEN') {
-            animateCamera();
+        switch (intersects[0].object.name) {
+            case 'FLOOR':
+            case 'FRIDGE':
+            case 'SCREEN':
+
+                switchStates();
+                break;
+
+            case 'BUTTON_1':
+                playIntroThenVideo(videos.video1);
+                animButton(intersects[0].object);
+                scene.remove(scene.getObjectByName("LINK_CONTACT"));
+                break;
+
+            case 'BUTTON_2':
+                playIntroThenVideo(videos.video2);
+                animButton(intersects[0].object);
+                scene.remove(scene.getObjectByName("LINK_CONTACT"));
+                break;
+
+            case 'BUTTON_3':
+                playIntroThenVideo(videos.video3);
+                animButton(intersects[0].object);
+                scene.remove(scene.getObjectByName("LINK_CONTACT"));
+                break;
+
+            case 'BUTTON_4':
+                playIntroThenVideo(videos.video4);
+                animButton(intersects[0].object);
+                scene.remove(scene.getObjectByName("LINK_CONTACT"));
+                break;
+
+            case 'BUTTON_5':
+                playIntroThenVideo(videos.video5);
+                animButton(intersects[0].object);
+                scene.remove(scene.getObjectByName("LINK_CONTACT"));
+                break;
+
+            case 'BUTTON_6':
+                playIntroThenVideo(videos.video6);
+                animButton(intersects[0].object);
+                scene.remove(scene.getObjectByName("LINK_CONTACT"));
+                break;
+
+            case 'BUTTON_CONTACT':
+                playIntroThenVideo(videos.videoContact);
+                animButton(intersects[0].object);
+
+                const geometry = new THREE.BoxGeometry(2.1, 1.4, 1);
+                const material = new THREE.MeshBasicMaterial({color: 0x00ff00, transparent: true, opacity: 0}); // Change color as needed
+                const linkContact = new THREE.Mesh(geometry, material);
+                linkContact.position.set(0.75, 2, -0.5);
+                linkContact.name = "LINK_CONTACT";
+
+                // Add the cube to the scene
+                 scene.add(linkContact);
+
+                break;
+
+            case 'LINK_CONTACT': 
+                window.open('https://many.bio/_', '_blank');
+                break;        
+
+            default:
+                break;
         }
-        else if (intersects[0].object.name.startsWith('BUTTON_')) {
-            playButtonAnimation(intersects[0].object);
-        }
+        
     }
 }
 
-function playButtonAnimation(button) {
-    
-    const action = mixer.clipAction(animations[1], button);
-    action.setLoop(THREE.LoopOnce);
-    action.clampWhenFinished = true;
-    if (!action.isRunning()) {
-        action.reset();
-        action.time = 0;
-        action.timeScale = 1;
-        action.play();
+
+function playIntroThenVideo(selectedVideo) {
+    if (!videos.intro.image.paused) {
+        videos.intro.image.pause();
+        videos.intro.image.currentTime = 0;
     }
-    video.currentTime = 0;
-    // Create a new video for the intro
-    const introVideo = document.createElement('video');
-    introVideo.src = 'video/intro.mp4';
-    introVideo.muted = true;
-    introVideo.play();
 
-    // Update the video and video texture
-    video = introVideo;
-    videoTexture.image = video;
-
-    video.onended = () => {
-        // Play a different video based on the button name
-        switch (button.name) {
-            case 'BUTTON_1':
-                video.src = 'video/video_1.mp4';
-                break;
-            case 'BUTTON_2':
-                video.src = 'video/video_2.mp4';
-                break;
-           
-            // Add more cases as needed
+    // Update the video texture on the GLTF model's screen to the intro video
+    scene.traverse(function(node) {
+        if (node.isMesh && node.material) {
+            if (node.name === "SCREEN") {
+                node.material.map = videos.intro;
+                node.material.needsUpdate = true;
+            }
         }
+    });
 
-        // Update the video texture
-        videoTexture.image = video;
+    videos.intro.image.play();
+    currentVideo = videos.intro;
+    videos.intro.image.onended = () => {
+        material.map = selectedVideo;
+        selectedVideo.image.currentTime = 0;  // Ensure the video starts from the beginning
+        selectedVideo.image.play();
+        currentVideo = selectedVideo;
 
-        video.play();
+        // Update the video texture on the GLTF model's screen to the selected video
+        scene.traverse(function(node) {
+            if (node.isMesh && node.material) {
+                if (node.name === "SCREEN") {
+                    node.material.map = selectedVideo;
+                    node.material.needsUpdate = true;
+                }
+            }
+        });
     };
 }
 
+
+function autoStart() {
+    
+    playIntroThenVideo(videos.video1);
+    
+    
+    loadModel();
+    
+    
+    
+    animate();
+}
+
+
 function animate() {
     requestAnimationFrame(animate);
+    
     const delta = clock.getDelta();
     if (mixer) mixer.update(delta);
-    if (isClose) {
-        camera.position.lerp(new THREE.Vector3(1.7, 1.8, 2.5), 0.03);
-        camera.quaternion.slerp(new THREE.Quaternion().setFromEuler(new THREE.Euler(0, 0, 0)), 0.05);
-    } else {
-        camera.position.lerp(new THREE.Vector3(0, 1, 8), 0.02);
-        camera.quaternion.slerp(new THREE.Quaternion().setFromEuler(new THREE.Euler(0, 0, 0)), 0.05);
-    }
 
-    // Update the video texture each frame FRAGWUERDIG
-    if (video.readyState === video.HAVE_ENOUGH_DATA) {
-        videoTexture.needsUpdate = true;
-    }
-    
+
+    camera.position.lerp(cameraTargetPosition, 0.02);
+
     renderer.render(scene, camera);
+    
 }
+
 
